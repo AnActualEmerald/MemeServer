f2a5ac640c6230e73871cc099acdbeeab76289ff
AnActualEmerald <kevinliger13@gmail.com>
2021-05-24 13:58:25 -0400

    Importing the pijul repo to git for safe keeping

---
+++ main.py
@@ -0,0 +1,103 @@
+# Written by AnActualEmerald
+# Last updated 5/20/21
+
+from http.server import BaseHTTPRequestHandler, HTTPServer
+from os import write, remove
+from urllib.parse import urlparse, parse_qs
+from urllib.request import urlopen, Request
+from datetime import date, datetime
+import time
+import whathow
+
+hostname = ""
+serverPort = 8080
+
+
+def load_bin(file):
+    with open(file, "rb") as file:
+        return file.read()
+
+
+def clean_files(file_list):
+    for file in file_list:
+        remove(file)
+
+
+class MemeServer(BaseHTTPRequestHandler):
+    def do_GET(self):
+        parsed_path = urlparse(self.path)
+        print(parsed_path.path)
+        if parsed_path.path == "/whathow":
+            print("Branching to whathow")
+            q_parts = parse_qs(parsed_path.query)
+            req = Request(q_parts['image'][0], headers={'User-Agent': 'Mozilla/5.0'})
+            img_path = f'./tmp/image_{datetime.now().microsecond}'
+            with open(img_path, mode='xb') as image:
+                with urlopen(req) as r:
+                    image.write(r.read())
+            video_path = whathow.gen_video(img_path)
+            self.send_response(200)
+            self.send_header("Content-type", "video/mp4")
+            self.end_headers()
+            self.wfile.write(
+                load_bin(video_path))
+            clean_files([img_path, video_path])
+        else:
+            print("dind't branch")
+            self.send_response(200)
+            self.send_header("Content-type", "text/html")
+            self.end_headers()
+            self.wfile.write(
+                bytes("<html><head><title>Meme server Post Response</title></head>", "utf-8"))
+            self.wfile.write(bytes("<body>", "utf-8"))
+            self.wfile.write(
+                bytes("<p>This will send you memes at some point</p>", "utf-8"))
+            self.wfile.write(bytes("</body></html>", "utf-8"))
+
+
+    def do_POST(self):
+        parsed_path = urlparse(self.path)
+        print(parsed_path.path)
+        if parsed_path.path == "/whathow":
+            print("Branching to whathow")
+            content_len = int(self.headers.get('Content-Length'))
+            post_body = self.rfile.read(content_len)
+            img_path = f'./tmp/image_{datetime.now().microsecond}'
+            with open(img_path, mode='xb') as image:
+                image.write(post_body)
+            video_path = whathow.gen_video(img_path)
+            self.send_response(200)
+            self.send_header("Content-type", "video/mp4")
+            self.end_headers()
+            self.wfile.write(
+                load_bin(video_path))
+            clean_files([img_path, video_path])
+        else:
+            print("dind't branch")
+            self.send_response(200)
+            self.send_header("Content-type", "text/html")
+            self.end_headers()
+            self.wfile.write(
+                bytes("<html><head><title>Meme server Post Response</title></head>", "utf-8"))
+            self.wfile.write(bytes("<body>", "utf-8"))
+            self.wfile.write(
+                bytes("<p>This will send you memes at some point</p>", "utf-8"))
+            self.wfile.write(bytes("</body></html>", "utf-8"))
+
+        # content_len = int(self.headers.get('Content-Length'))
+        # post_body = self.rfile.read(content_len)
+
+        # print(post_body)
+
+
+if __name__ == "__main__":
+    webServer = HTTPServer((hostname, serverPort), MemeServer)
+    print("Starting server at http://%s:%s" % (hostname, serverPort))
+
+    try:
+        webServer.serve_forever()
+    except KeyboardInterrupt:
+        pass
+
+    webServer.server_close()
+    print("Stopped server")
---
+++ whathow.py
@@ -0,0 +1,53 @@
+from datetime import datetime
+from moviepy.editor import *
+
+
+def gen_video(image_path):
+    resolution = (640, 480)
+
+    # Load sound (should start at 20 seconds)
+    snd_clip = AudioFileClip(
+        "./res/whathowsound.mp4").subclip(20, 30.15)
+
+    # Generate text --- THIS DOESN'T WORK WITH SOME VERSIONS OF IMAGEMAGICK ---
+    what_txt = TextClip(txt="WHAT", font="Bitstream-Vera-Serif-Bold", color="white",
+        kerning=2, align='north', fontsize=50).set_duration(snd_clip.duration)
+    how_txt = TextClip(txt="HOW", font="Bitstream-Vera-Serif", color="white",
+        kerning=2, align='north', fontsize=24).set_duration(snd_clip.duration)
+
+    # Load image
+    img_clip = ImageClip(image_path).set_pos("center").set_fps(24)
+
+    # Determine how much to scale the image based on the original size
+    scale_x = resolution[0] / img_clip.w
+    scale_y = resolution[1] / img_clip.h
+
+    scale = scale_x if scale_x < scale_y else scale_y
+    print(f"Original res is {img_clip.size}, scaling by {scale}")
+
+    final_scale = 0.5 if scale <= 1.0 else scale - scale.__floor__()
+
+    big_clip = img_clip.resize(scale)
+    tmp = big_clip.resize(final_scale)
+    small_clip = tmp.set_pos(("center", ((resolution[1] - tmp.h) / 2) - 75))\
+        .margin(mar=10).margin(mar=3, color=(255, 255, 255))
+
+    # Composite output video
+
+    target = './tmp'
+
+    file_name = 'what_how_%s.mp4' % datetime.now().microsecond
+
+    output = CompositeVideoClip([big_clip.set_end(1), small_clip.set_start(
+        1), what_txt.set_pos(("center", 0.73), relative=True).set_start(1), how_txt.set_pos(("center", 0.85), relative=True).set_start(1)], size=resolution).set_audio(snd_clip).set_duration(snd_clip.duration)
+    output.write_videofile(
+        f"{target}/{file_name}", audio_bitrate='160k')
+
+    img_clip.close()
+    snd_clip.close()
+    what_txt.close()
+    how_txt.close()
+    output.close()
+
+    print(f'Output whathow video at {target}/{file_name}')
+    return f'{target}/{file_name}'
